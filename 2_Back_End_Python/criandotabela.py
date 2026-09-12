2cdc028652fc6a7a399c76cd57a60d84fda50f7b
Bruno Cruvinel Barros da Silva <bruno.cruvinel07@gmail.com>
2026-09-12 20:57:15 -0300

    Feat: Começamdo estudos em pythin pandas

--- 2_Back_End_Python/criandotabela.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-# DataFrame é uma estrutua de dados,semelhante a uma tabela.
-dados = {
-    "nome": ["Ana", "Bruno", "Carlos"],
-    "idade": [23, 31, 29]
-}
-
-df = pd.DataFrame(dados)
-print(df)
